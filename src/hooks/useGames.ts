cf4e464beaa9e07d2512ab7a5e78b2c373901b3b
Joseph Kirkish <kirkish.2@gmail.com>
2024-01-23 15:49:21 -0500

    Refactoring-Extracting a Query Object

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 
 export interface Platform{
@@ -20,11 +20,13 @@ export interface Game {
   
 
 
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => 
+const useGames = (
+  gameQuery: GameQuery
+) => 
 useData<Game>('/games', {
-  params: {genres: selectedGenre?.id, 
-  platforms: selectedPlatform?.id}},
-[selectedGenre?.id, selectedPlatform?.id
+  params: {genres: gameQuery.genre?.id, 
+  platforms: gameQuery.platform?.id}},
+[gameQuery
 ]);
 
 
