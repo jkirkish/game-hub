9589422159ee3df48ab20ea463f8304ded1fa700
Joseph Kirkish <kirkish.2@gmail.com>
2024-01-24 20:10:48 -0500

    Sorting the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -25,9 +25,13 @@ const useGames = (
 ) => 
 useData<Game>('/games', {
   params: {genres: gameQuery.genre?.id, 
-  platforms: gameQuery.platform?.id}},
-[gameQuery
-]);
+  platforms: gameQuery.platform?.id,
+  ordering: gameQuery.sortOrder
+  },
+},
+[gameQuery]
+
+);
 
 
 export default useGames;
