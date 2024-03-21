54a4b03eb7baa296a364f718518f60cb72afb1a7
Joseph Kirkish <kirkish.2@gmail.com>
2024-03-20 21:10:28 -0400

    fetch games with React QUery

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-export default axios.create({
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
-    baseURL: 'https://api.rawg.io/api',
-    params:{
-        key: '64d7d723aa8c47ce8f0d4253f7645abb'
-    }
-})
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "64d7d723aa8c47ce8f0d4253f7645abb",
+  },
+});
